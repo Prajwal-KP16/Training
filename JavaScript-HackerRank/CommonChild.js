d4d6d5e4b9292c36072a53967268205536b9da04
Prajwal-KP16 <prajwalkpdscv@gmail.com>
2025-06-12 23:48:49 +0530

    Javascript Day: 19

--- JavaScript-HackerRank/CommonChild.js
+++ JavaScript-HackerRank/CommonChild.js
@@ -1,11 +1,11 @@
 function commonChild(s1, s2) {
-  let arr = [Array(s2.length + 1).fill(0)];
+  let table = [Array(s2.length + 1).fill(0)];
   [...s1].forEach((v1, i) => {
-    arr[i + 1] = [0];
+    table[i + 1] = [0];
     [...s2].forEach((v2, j) => {
-      arr[i + 1][j + 1] =
-        v1 === v2 ? arr[i][j] + 1 : Math.max(arr[i + 1][j], arr[i][j + 1]);
+      table[i + 1][j + 1] =
+        v1 === v2 ? table[i][j] + 1 : Math.max(table[i + 1][j], table[i][j + 1]);
     });
   });
-  return arr[s2.length][s1.length];
+  return table[s2.length][s1.length];
 }
