70164574ec52599be21709e03b7b40b712749190
Prajwal-KP16 <prajwalkpdscv@gmail.com>
2025-08-20 23:33:56 +0530

    Reactjs Mega Project Day:58

---
+++ Reactjs/MegaProject/src/components/AuthLayout.jsx
@@ -0,0 +1,29 @@
+//this is the type of mechanism to protect pages ot routes
+//this is a protected container
+
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status); //asking from authStatus that, logged in or not. we dont directly depend on user, we will ask store
+
+  useEffect(() => {
+    // if(authStatus === true){
+    //     navigate("/")
+    // }else if(authStatus === false){
+    //     navigate('/login')
+    // }
+
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
+}
--- Reactjs/MegaProject/src/components/index.js
+++ Reactjs/MegaProject/src/components/index.js
@@ -1,4 +1,8 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container.jsx";
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
+import RTE from "./RTE.jsx";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutBtn, RTE };
